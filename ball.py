24b67c969ef7fe83e5a0af87d717404bd885203b
gopi-fsui <gopi.fsui@gmail.com>
2026-03-08 22:15:01 +0530

    Create ball.py

---
+++ ball.py
@@ -0,0 +1,20 @@
+from turtle import Turtle
+import random
+
+ANGLE = [0,30,150,180,210,330]
+
+class Ball(Turtle):
+    def __init__(self,height:int):
+        super().__init__("circle")
+        self.half_height = int(height/2)
+        self.color("black")
+        self.penup()
+
+
+    def start_pos(self):
+        self.teleport(0,random.randint(-int(self.half_height*0.75),int(self.half_height*0.75)))
+        self.setheading(random.choice(ANGLE))
+
+
+    def move(self):
+        self.fd(5)
