f0552e8511318346ab8cc1da2e59b08243341df6
gopi-fsui <gopi.fsui@gmail.com>
2026-03-08 22:51:08 +0530

    Update ball.py
    added reflection method for ball when hit edges

--- ball.py
+++ ball.py
@@ -10,11 +10,12 @@ class Ball(Turtle):
         self.color("black")
         self.penup()
 
-
     def start_pos(self):
         self.teleport(0,random.randint(-int(self.half_height*0.75),int(self.half_height*0.75)))
         self.setheading(random.choice(ANGLE))
 
-
     def move(self):
         self.fd(5)
+        
+    def reflection(self):
+        self.setheading(360-self.heading())
