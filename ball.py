9d9afe5e715a685360a4659a95d9047f15177ab6
gopi-fsui <gopi.fsui@gmail.com>
2026-03-09 19:40:13 +0530

    Update ball.py

--- ball.py
+++ ball.py
@@ -9,13 +9,24 @@ class Ball(Turtle):
         self.half_height = int(height/2)
         self.color("black")
         self.penup()
+        self.shapesize(0.5)
 
     def start_pos(self):
         self.teleport(0,random.randint(-int(self.half_height*0.75),int(self.half_height*0.75)))
         self.setheading(random.choice(ANGLE))
 
     def move(self):
-        self.fd(5)
-        
+        self.fd(10)
+
     def reflection(self):
         self.setheading(360-self.heading())
+        self.fd(10)
+
+    def paddle_ref(self):
+        if self.heading() == 0 :
+            self.setheading(random.choice([30,150]))
+        elif self.heading() == 180 :
+            self.setheading(random.choice([330,210]))
+        self.setheading(540-self.heading())
+        self.fd(10)
+        
